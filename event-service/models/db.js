11fba06e694b3d8e17fbe188c690413e43dbea31
HamzaSidd17 <hamzaasiddiqui21@gmail.com>
2025-03-11 14:15:26 +0500

    Final Update

--- event-service/models/db.js
+++ event-service/models/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.DB_URL)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log("Error: " + err));
 
